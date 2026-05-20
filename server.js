d42401696485ced21568f540f78ff939e3017dfe
DevTest52-stack <testawsesemail@gmail.com>
2026-05-20 15:49:19 +0530

    req err

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const arr=[];
 app.use(express.text({ type: '*/*' })); 
 
 app.post('/',(req,res)=>{
-    console.log(req)
-    console.log(req.body)
-    // const {data}=req.body;
+    // console.log(req)
+    //console.log(req.body)
+    const {data}=req.body;
     // console.log(data);
     // arr.push(data);  
     res.send("Got a post response data:")

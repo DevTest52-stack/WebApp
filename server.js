1dfb45aecaf752f22e873970505c959e62a3d322
DevTest52-stack <testawsesemail@gmail.com>
2026-05-20 14:39:17 +0530

    req.body error

--- server.js
+++ server.js
@@ -1,18 +1,70 @@
+const WebSocket = require('ws')
 const express = require('express')
+const http = require('http')
 const app = express()
 const port = 3000
+
+const arr=[];
 
 app.use(express.json());
 
 app.post('/',(req,res)=>{
-    // console.log("request hit")
-    const {data}=req.body;
-    // console.log(req.body);
-    console.log(data);
-    res.send("Got a post response");
+    console.log(req.body)
+    // const {data}=req.body;
+    // console.log(data);
+    // arr.push(data);
+    res.send("Got a post response data:")
+    //res.send(`Got a post response data: ${data}`);
 })
 
-app.listen(port, ()=>{
-    console.log(`Server running on Port:${port}`)
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+// wss.on('connection', ws => {
+//     console.log('Client connected');
+
+//     // Handle messages from the client (e.g., from the Qt app)
+//     ws.on('message', message => {
+//         let messageData=message.toString();
+//         let messageDataParts=messageData.split(",")
+//         let timeStamp=messageDataParts[0]
+//         let lat=messageDataParts[1]
+//         let lng=messageDataParts[2]
+//         //console.log(`Received message: ${message}`);
+//         console.log(`${lat},${lng},${Date(timeStamp)}`)
+
+//         // Send a response back to the client
+//         ws.send(`${message}`);
+//         //send to all clients 
+//         wss.clients.forEach(client => {
+//             if (client !== ws && client.readyState === WebSocket.OPEN) {
+//                 client.send(`${message}`);
+//             }
+//         });
+//     });
+
+//     ws.on('close', () => {
+//         console.log('Client disconnected');
+//     });
+
+//     // Send data periodically to simulate location updates
+//     // const interval = setInterval(() => {
+//     //     // const latitude = Math.random() * 180 - 90;
+//     //     // const longitude = Math.random() * 360 - 180;
+//     //     lat=parseFloat(lat+0.0001)
+//     //     lng=parseFloat(lng+0.0001)
+//     //     const data = `${lat.toFixed(4)},${lng.toFixed(4)}`;
+//     //     ws.send(data);
+//     // }, 2000);
+
+//     ws.on('close', () => {
+//         //clearInterval(interval); // Stop sending data when client disconnects
+//         console.log('socket closed')
+//     });
+// });
+
+server.listen(port, ()=>{
+    console.log(`Server running on Port:${port}`);
 })
 

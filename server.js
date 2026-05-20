63a348fe7bcd7b715e7aa5ce5939b067cd450b77
DevTest52-stack <testawsesemail@gmail.com>
2026-05-20 12:20:00 +0530

    post req added

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.post('/',(req,res)=>{
+    // console.log("request hit")
+    const {data}=req.body;
+    // console.log(req.body);
+    console.log(data);
     res.send("Got a post response");
 })
 
